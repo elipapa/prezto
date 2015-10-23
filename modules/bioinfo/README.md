0f6acb702d005b754e93f0811a46288e3e8bef41
Eliseo Papa <elipapa@alum.mit.edu>
2015-10-23 13:00:20 +0100

    bioinfo module

---
+++ modules/bioinfo/README.md
@@ -0,0 +1,25 @@
+Bioinfo
+=======
+
+Loads aliases and commands to deal with fasta, bam, sam files.
+
+Options
+-------
+
+
+Aliases
+-------
+
+  - `fac` counts the number of sequences in a fasta file.
+  - `fah` shows all headers in a fasta file.
+
+
+
+Authors
+-------
+
+*The authors of this module should be contacted via the [issue tracker][1].*
+
+  - [Eliseo Papa](http://elipapa.github.io)
+
+[1]: https://github.com/sorin-ionescu/prezto/issues
---
+++ modules/bioinfo/init.zsh
@@ -0,0 +1,18 @@
+# from http://userweb.eng.gla.ac.uk/umer.ijaz/bioinformatics/oneliners.html
+#
+#print all headers
+alias fah='grep "^>" $1'
+
+#count sequences
+alias fac='grep -c "^>" $1'
+
+# fasta to tabs
+
+# 	https://github.com/stephenturner/oneliners
+# 	http://bioinformatics.cvr.ac.uk/blog/short-command-lines-for-manipulation-fastq-and-fasta-sequence-files/
+# 	http://maojf.blogspot.co.uk/2012/02/useful-bash-commands-to-handle-fasta.html?m=1
+# 	https://gist.github.com/renzok/c34eea06a86c78c52366
+# 	https://github.com/mdshw5/pyfaidx
+# 	https://www.biostars.org/p/17680/
+# https://github.com/maasha/biopieces/wiki
+# %
